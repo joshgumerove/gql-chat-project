ef0281b24db7edeb734bfb1cf0679b998484c2d6
Josh Gumerove <jgumerove@kaufmanrossin.com>
2023-07-23 16:35:41 -0400

    updateQuery cache

--- client/src/lib/graphql/hooks.js
+++ client/src/lib/graphql/hooks.js
@@ -1,12 +1,28 @@
-import { useMutation, useQuery } from '@apollo/client';
-import { addMessageMutation, messagesQuery } from './queries';
+import { useMutation, useQuery } from "@apollo/client";
+import { addMessageMutation, messagesQuery } from "./queries";
 
 export function useAddMessage() {
   const [mutate] = useMutation(addMessageMutation);
 
   const addMessage = async (text) => {
-    const { data: { message } } = await mutate({
+    const {
+      data: { message },
+    } = await mutate({
       variables: { text },
+      update: (cache, { data }) => {
+        console.log("[useAddMessage] data: ", data);
+        const newMessage = data.message;
+        cache.updateQuery(
+          {
+            query: messagesQuery,
+          },
+          (oldData) => {
+            return {
+              messages: [...oldData.messages, newMessage],
+            };
+          }
+        );
+      },
     });
     return message;
   };
